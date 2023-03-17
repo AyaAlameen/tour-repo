21a5f279b6c8bf5e5de4fbbb9c5774cd7ca5c269
Bayan-Abbas <bs.aa.yb.ab.na@gmail.com>
2023-03-17 21:24:23 +0200

    app

--- routes/web.php
+++ routes/web.php
@@ -81,3 +81,6 @@ Route::get('/home', function () {
 Route::get('/about', function () {
     return view('user.about');
 })-> name('about');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
