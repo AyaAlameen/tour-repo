657a3694b65a4f42a971710397bfdc44931bc374
AyaAlameen <uualameen111@gmail.com>
2023-03-24 20:45:30 +0200

    favorites

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -432,7 +432,7 @@
                         @csrf
 
                         <div class="form-group d-flex first align-items-center">
-                            <label for="name"   class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="name"   class="col-md-4 text-center col-form-label text-md-end">{{ __('Name') }}</label>
 
                             
                                 <input id="name" placeholder="eg.Aya Alameen" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -446,7 +446,7 @@
                         </div>
 
                         <div class="form-group d-flex align-items-center">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
+                            <label for="email" class="col-md-4 text-center col-form-label text-md-end">{{ __('Email') }}</label>
 
                             
                                 <input id="email" placeholder="your-email@gmail.com" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -460,7 +460,7 @@
                         </div>
 
                         <div class="form-group d-flex align-items-center">
-                            <label for="password"   class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password"   class="col-md-4 text-center col-form-label text-md-end">{{ __('Password') }}</label>
 
                            
                                 <input id="password" placeholder="Your Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -474,7 +474,7 @@
                         </div>
 
                         <div class="form-group last d-flex align-items-center">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 text-center col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                        
