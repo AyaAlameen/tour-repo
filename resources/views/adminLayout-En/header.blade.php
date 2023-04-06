338cd6427a69157921c4d5c9f2ea427c2445f36e
AyaAlameen <uualameen111@gmail.com>
2023-04-06 17:48:45 +0300

    translate

--- resources/views/adminLayout-En/header.blade.php
+++ resources/views/adminLayout-En/header.blade.php
@@ -28,32 +28,94 @@
     <!-- Topbar End -->
 
 
-    <!-- Navbar Start -->
-    <div class="container-fluid position-relative nav-bar bg-light p-0">
-        <div class="container-lg position-relative p-0 px-lg-3 bg-light" style="z-index: 9;">
+      <!-- Navbar Start -->
+      <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
             <nav class="navbar navbar-expand-lg bg-light navbar-light shadow-lg py-3 py-lg-0 pl-3 pl-lg-5">
                 <a href="" class="navbar-brand">
-                    <h1 class="m-0" style="color : var(--bambi);"><span class="text-dark">TRAVEL</span>ER</h1>
+                    <h1 class="m-0" style="color:var(--bambi);"><span class="text-dark">TRAVEL</span>ER</h1>
                 </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
                     <div class="navbar-nav ml-auto py-0">
-                    <a href="index.html" class="nav-item nav-link active">DashBoard</a>
-                        <a href="{{ route ('userhome') }}" class="nav-item nav-link text-primary">Home</a>
-                        <a href="about.html" class="nav-item nav-link text-primary">About</a>
-                        <a href="service.html" class="nav-item nav-link text-primary">Services</a>
-                        <a href="package.html" class="nav-item nav-link text-primary">Tour Packages</a>
+                        <a href="{{route('userhome')}}" class="nav-item nav-link active">Home</a>
+                     
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Syria</a>
+                            <div class="dropdown-menu border-0 rounded-0 m-0">
+                                <a href="blog" class="dropdown-item">Visit Syria</a>
+                                <a href="blog" class="dropdown-item">Governorates of Syria</a>
+                                <a href="single" class="dropdown-item">Syria Hotels</a>
+                                <a href="destination" class="dropdown-item">Syria Restaurants</a>
+                                <a href="guide" class="dropdown-item">Historical places in Syria</a>
+                                <a href="guide" class="dropdown-item">Trip groups</a>
+                                <a href="testimonial" class="dropdown-item">Photographs from Syria</a>
+                            </div>
+                        </div>
+                        <a href="{{route('about')}}" class="nav-item nav-link">About</a>
 
-                        <a href="contact.html" class="nav-item nav-link text-primary">Contact</a>
+                       
+                        <a href="contact" class="nav-item nav-link">Contact</a>
+                       <!-- Authentication Links -->
+                        @guest
+                            <!-- @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif -->
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif 
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                       <a class="nav-item nav-link"> <i class="fas fa-heart heart" title="favorite" onClick="getFavorite()" style=" color:var(--bambi);  cursor: pointer;"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"></i></a>
+
                     </div>
                 </div>
             </nav>
         </div>
     </div>
-    <!-- Navbar End -->
 
+
+    <!-- favorite --> 
+ 
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h3 id="offcanvasRightLabel" class="text-primary">Favorites :</h3>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <img src="img/folder.png" width="130px" height="130px" style="margin-left:125px; margin-top:160px;"/>
+    <p class="text-body px-3 text-center mt-4">choose your favorite places</p>
+
+  </div>
+</div>
+ <!-- end favorite -->
+
+    <!-- Navbar End -->
 <!-- partial:index.partial.html -->
 <div class="app-container">
   <div class="sidebar" style="overflow-y: scroll;">
